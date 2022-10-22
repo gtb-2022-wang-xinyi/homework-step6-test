0d650c1ae9cf9bc315d3cbb25fe532784e5a0b71
{gtb-2022-wang-xinyi} <841059462@qq.com>
2022-10-22 23:34:49 +0800

    feat: add test for space task name

--- app/src/test/java/com/tw/cn/cap/gtb/todo/TaskFactoryTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/TaskFactoryTest.java
@@ -2,13 +2,23 @@ package com.tw.cn.cap.gtb.todo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class TaskFactoryTest {
 
     @Test
+    void should_support_name_with_multiple_white_space() {
+        final var task = TaskFactory.createTask(1, "+    foo  bar  ");
+        assertEquals("   foo  bar  ", task.getName());
+    }
+
+    @Test
     void should_parse_completed_property_for_task() {
         final var isCompleted = TaskFactory.createTask(1,"+ task").isCompleted();
         assertFalse(isCompleted);
+        
+        
     }
+
 }
