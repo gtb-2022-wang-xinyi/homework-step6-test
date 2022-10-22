a335f83bda7117965d7f4dd9d7f58a968cfc7882
{gtb-2022-wang-xinyi} <841059462@qq.com>
2022-10-22 16:00:11 +0800

    refactor: extract TaskRepository

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,44 +1,23 @@
 package com.tw.cn.cap.gtb.todo;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+
+    private final TaskRepository taskRepository = new TaskRepository();
 
     public static void main(String[] args) {
         throw new UnsupportedOperationException();
     }
 
     public List<String> run() {
-        final List<Task> tasks = loadTasks();
+        final List<Task> tasks = taskRepository.loadTasks();
         final List<String> result = new ArrayList<>();
         result.add("# To be done");
         for (var task : tasks){
             result.add(task.formate());
         }
         return result;
-    }
-
-    private List<Task> loadTasks() {
-        final List<String> lines = readTaskLines();
-        final List<Task> tasks = new ArrayList<>();
-        for (int i = 0; i < lines.size(); i++){
-            final var id = i + 1;
-            final var line = lines.get(i);
-            final var name = line;
-            final var task = new Task(id, name);
-            tasks.add(task);
-        }
-        return tasks;
-    }
-
-    private List<String> readTaskLines() {
-        try {
-            return Files.readAllLines(Constants.TASKS_FILE_PATH);
-        } catch (IOException e) {
-            throw new TodoCannotReadFileException();
-        }
     }
 }
