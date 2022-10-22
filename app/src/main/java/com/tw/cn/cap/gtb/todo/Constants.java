3a3322069cfe3650add1b7945d5d56ffd1a985fc
{gtb-2022-wang-xinyi} <841059462@qq.com>
2022-10-22 11:02:16 +0800

    refactor: extract Constants class

---
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/Constants.java
@@ -0,0 +1,11 @@
+package com.tw.cn.cap.gtb.todo;
+
+import java.nio.file.Path;
+
+public class Constants {
+    private Constants(){}
+    public static final String USER_HOME_PATH = System.getProperty("user.name");
+    public static final String TODO_CONFIG_PATH = ".todo";
+    public static final String TASKS_FILENAME = "tasks";
+    public static final Path TASKS_FILE_PATH = Path.of(USER_HOME_PATH, TODO_CONFIG_PATH, TASKS_FILENAME);
+}
